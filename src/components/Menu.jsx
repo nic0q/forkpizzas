de13cadf1a4b46552a6911194315826e08088585
Nícolas Farfán <nicolas.farfan.ch@usach.cl>
2022-12-05 13:44:28 -0300

    [feat]: Loader menu apge

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -12,7 +12,8 @@ import add_quantity from '../utilities/add_quantity';
 export default function Menu() {
   const [cart, setCart] = useState(localStorage.getItem("cart") ? JSON.parse(localStorage.getItem("cart")) : []);
   const [total, setTotal] = useState(0);
-
+  const [loading, setLoading] = useState(false);
+  
   useEffect(() => {
     try {
       let total = 0
@@ -25,6 +26,14 @@ export default function Menu() {
       setTotal(0);
     }
   }, [cart])
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+  }, []);
+
   const add = (name, id, img, price, ingredients = []) => {
     setCart(add_to_cart({name, ingredients, id, img, price}));
   }
@@ -41,35 +50,40 @@ export default function Menu() {
     setCart(add_quantity(id))
   }
   return (
-    <div className="flex flex-col overflow-x-hidden">
+    <div>
+{loading ? (
+ "Loading..."
+      ) : <div className="flex flex-col overflow-x-hidden">
       <Navbar n_cart_items = {cart.length !== 0 ? cart.items.length : 0}></Navbar>
-    
-    <div className="flex flex-row w-screen bg-[#F5F5F5] text-black justify-center pt-8 pb-32">
-      <div className="flex flex-col w-[65%] mr-3">
-        <div className="flex justify-around border-2 border-gray-400 rounded-2xl py-3">
-          <div ><a href="#pizzas">Pizzas</a></div>
-          <div><a href="#bebidas">Bebidas</a></div>
-          <div><a href="#complementos">Complementos</a></div>
+      <div className="flex flex-row w-screen bg-[#F5F5F5] text-black justify-center pt-8 pb-32">
+        <div className="flex flex-col w-[65%] mr-3">
+          <div className="flex justify-around border-2 border-gray-400 rounded-2xl py-3">
+            <div ><a href="#pizzas">Pizzas</a></div>
+            <div><a href="#bebidas">Bebidas</a></div>
+            <div><a href="#complementos">Complementos</a></div>
+          </div>
+          <div className="border-2 border-gray-400 rounded-2xl my-3">
+            <div className="flex flex-row justify-center" id="pizzas">
+              {products.Pizzas.map((pizza) => <FoodCard key={pizza.id} id = {pizza.id} name={pizza.name} ingredients={pizza.ingredients} price={pizza.price}  img = {pizza.img} add = {()=>add(pizza.name, pizza.id, pizza.img, pizza.price, pizza.ingredients)} size = {"w-48 "}/>)}
+            </div>
+            <div className="flex flex-row justify-center" id="bebidas">
+              {products.Bebidas.map((item) => <FoodCard key={item.id} id = {item.id} name={item.name} price={item.price} img={item.img} add = {()=>add(item.name, item.id, item.img, item.price)} size = {"w-32"}/>)}
+            </div>
+            <div className="flex flex-row justify-center" id="complementos">
+              {products.Complements.map((item) => <FoodCard key={item.id} id = {item.id} name={item.name} price={item.price} img={item.img} add = {()=>add(item.name, item.id, item.img, item.price)} size = {"w-48 pb-3"}/>)}
+            </div>
+          </div>
         </div>
-        <div className="border-2 border-gray-400 rounded-2xl my-3">
-          <div className="flex flex-row justify-center" id="pizzas">
-            {products.Pizzas.map((pizza) => <FoodCard key={pizza.id} id = {pizza.id} name={pizza.name} ingredients={pizza.ingredients} price={pizza.price}  img = {pizza.img} add = {()=>add(pizza.name, pizza.id, pizza.img, pizza.price, pizza.ingredients)} size = {"w-48 "}/>)}
-          </div>
-          <div className="flex flex-row justify-center" id="bebidas">
-            {products.Bebidas.map((item) => <FoodCard key={item.id} id = {item.id} name={item.name} price={item.price} img={item.img} add = {()=>add(item.name, item.id, item.img, item.price)} size = {"w-32"}/>)}
-          </div>
-          <div className="flex flex-row justify-center" id="complementos">
-            {products.Complements.map((item) => <FoodCard key={item.id} id = {item.id} name={item.name} price={item.price} img={item.img} add = {()=>add(item.name, item.id, item.img, item.price)} size = {"w-48 pb-3"}/>)}
-          </div>
+        <div>
+  
         </div>
+        
+        <Cart add_item = {add_item} remove_one_item = {remove_one_item} carte = {cart} remove_item = {remove_item} empty_cart = {empty_cart} total = {total}/>
       </div>
-      <div>
-
-      </div>
+        <Footer/>
+      </div> }
+    </div>
       
-      <Cart add_item = {add_item} remove_one_item = {remove_one_item} carte = {cart} remove_item = {remove_item} empty_cart = {empty_cart} total = {total}/>
-    </div>
-      <Footer/>
-    </div>
+      
   )
 }
