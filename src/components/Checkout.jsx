d481ebf3707ec8d7ad2a89248857d1d7a7344917
Nícolas Farfán <nicolas.farfan.ch@usach.cl>
2022-12-06 20:12:44 -0300

    [refactor]: Resize elements checkout view

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -50,54 +50,65 @@ export default function Checkout(){
     setDeliveryCost(cost);
   };
   const ProductCart = ({name, quantity, price, img, id}) =>{
-    return  <div className="flex shrink-0 grow-0 w-full h-[100px] my-3 bg-gray-100 rounded-xl items-center justify-between pr-4 text-gray-800 shadow-xl drop-shadow-md">
-    <img className="w-[100px] ml-10 h-[90%]" alt = "pizza" src={img}></img>
+    return  <div className="flex shrink-0 grow-0 w-full h-[60px] my-3 bg-gray-100 rounded-xl items-center justify-between pr-4 text-gray-800 shadow-xl drop-shadow-md">
+    <img className="w-[60px] ml-10 h-[90%]" alt = "pizza" src={img}></img>
     <div className='flex flex-col w-[35%]'>
-      <h3 className="text-2xl">{name}</h3>
-      <h3 className="text-lg mt-2 text-gray-500">{to_clp(price)}</h3>
+      <h3 className="text-xl">{name}</h3>
+      <h3 className="text-lg  text-gray-500">{to_clp(price)}</h3>
     </div>
     <div className="flex flex-row text-center align-middle justify-between items-center w-[15%]">
       <div className="btn" onClick={()=>remove_one_item(id)}>-</div>
-      <div className="mx-3 w-10 text-3xl">{quantity}</div>
+      <div className="mx-3 w-10 text-xl">{quantity}</div>
       <div className="btn" onClick={()=>add_item(id)}>+</div>
     </div>
     <div className="flex flex-col items-center w-[10%]">
-      <div className="text-2xl">{to_clp(price * quantity)}</div>
+      <div className="text-xl">{to_clp(price * quantity)}</div>
       <div className="text-red-500 text-sm underline decoration-wavy cursor-pointer" onClick={()=>remove_item(id)}>Eliminar</div>
     </div>
   </div>
   }
   return <div className="flex bg-gray-100 h-screen items-center justify-center">
     <div className="flex flex-col w-[30%] h-[90%] m-6">
+      <h4>Datos</h4>  
+      <div className='flex flex-col bg-white shadow-xl drop-shadow-md h-[20%] rounded-xl justify-around items-center mb-6'>
+        <div className={`w-[90%] h-20 flex items-center`}>
+          <p className='text-sm w-[30%] mr-8'>Ingrese su nombre</p>
+          <input type="text" placeholder='Nombre' className='w-[70%] border-2 rounded-lg shadow-xl drop-shadow-md font-sans'></input>
+        </div>
+        <div className={`w-[90%] h-20 flex items-center`}>
+        <p className='text-sm w-[30%] mr-8'>Ingrese su correo</p>
+          <input type="email" placeholder='Email' required className='w-[70%] border-2 rounded-lg shadow-xl drop-shadow-md font-sans'></input>
+        </div>
+      </div>
       <h4>Método de entrega</h4>  
-      <div className='flex flex-col bg-white shadow-xl drop-shadow-md h-[60%] rounded-xl justify-around items-center mb-6'>
-      <div onClick={()=>handleChangeDelivery(0, 0)} className={`w-72 h-20 justify-evenly flex items-center border-2 rounded-xl shadow-xl drop-shadow-md ${delivery === 0 ? "bg-orange-300 " : 'bg-white cursor-pointer'}`}>
+      <div className='flex flex-col bg-white shadow-xl drop-shadow-md h-[25%] rounded-xl justify-around items-center mb-6'>
+      <div onClick={()=>handleChangeDelivery(0, 0)} className={`w-72 h-20 my-2 justify-evenly flex items-center border-2 rounded-xl shadow-xl drop-shadow-md ${delivery === 0 ? "bg-orange-300 " : 'bg-white cursor-pointer'}`}>
           <p>Entrega en Local</p>
           <p className='text-3xl'>🏬</p>
         </div>
-        <div onClick={()=>handleChangeDelivery(1, 2500)} className={`w-72 h-20 justify-evenly flex items-center border-2 rounded-xl shadow-xl drop-shadow-md ${delivery === 1 ? "bg-red-300 " : 'bg-white cursor-pointer'}`}>
+        <div onClick={()=>handleChangeDelivery(1, 2500)} className={`w-72 h-20 my-2 justify-evenly flex items-center border-2 rounded-xl shadow-xl drop-shadow-md ${delivery === 1 ? "bg-red-300 " : 'bg-white cursor-pointer'}`}>
           <p>a Domicilio</p>
           <p className='text-3xl'>🏠</p>
         </div>
       </div>
       <h4>Seleccione el medio de pago</h4>  
-      <div className='flex flex-col bg-white shadow-xl drop-shadow-md h-[35%] rounded-xl justify-between items-center'>
-        <div onClick={()=>setPayment(0)} className={`w-72 h-20 justify-evenly flex items-center border-2 rounded-xl shadow-xl drop-shadow-md ${payment === 0 ? "bg-green-300 " : 'bg-white cursor-pointer'}`}>
+      <div className='flex flex-col bg-white shadow-xl drop-shadow-md h-[30%] rounded-xl justify-between items-center'>
+        <div onClick={()=>setPayment(0)} className={`w-72 h-20 my-2 justify-evenly flex items-center border-2 rounded-xl shadow-xl drop-shadow-md ${payment === 0 ? "bg-green-300 " : 'bg-white cursor-pointer'}`}>
           <p>Efectivo</p>
           <p className='text-3xl'>💵</p>
         </div>
-        <div onClick={()=>setPayment(1)} className={`w-72 h-20 justify-evenly flex items-center border-2 rounded-xl shadow-xl drop-shadow-md ${payment === 1 ? "bg-blue-300 " : 'bg-white cursor-pointer'}`}>
+        <div onClick={()=>setPayment(1)} className={`w-72 h-20 my-2 justify-evenly flex items-center border-2 rounded-xl shadow-xl drop-shadow-md ${payment === 1 ? "bg-blue-300 " : 'bg-white cursor-pointer'}`}>
           <p>Targeta de Crédito</p>
           <p className='text-3xl'>💳</p>
         </div>
-        <div onClick={()=>setPayment(2)} className={`w-72 h-20 justify-evenly flex items-center border-2 rounded-xl shadow-xl drop-shadow-md ${payment === 2 ? "bg-yellow-300 " : 'bg-white cursor-pointer'}`}>
+        <div onClick={()=>setPayment(2)} className={`w-72 h-20  my-2 justify-evenly flex items-center border-2 rounded-xl shadow-xl drop-shadow-md ${payment === 2 ? "bg-yellow-300 " : 'bg-white cursor-pointer'}`}>
           <p>WebPay</p>
           <p className='text-3xl'>🏦</p>
         </div>
       </div>
     </div>
-    <div className="flex flex-col w-[70%] h-[90%] mr-6 ">
-      <div className="flex flex-col flex-nowrap bg-white rounded-xl w-full h-[90%] p-4 overflow-y-auto mb-6 shadow-xl drop-shadow-md">
+    <div className="flex flex-col w-[70%] h-full pt-8 mr-6 ">
+      <div className="flex flex-col flex-nowrap bg-white rounded-xl w-full h-full p-4 overflow-y-auto mb-6 shadow-xl drop-shadow-md">
         <div className="flex justify-between">
           <div className='flex flex-row text-start items-center mt-[-10px] cursor-pointer text-green-500' onClick={()=>navigate("/forkpizzas")}>
             <BsFillArrowLeftCircleFill className='w-8 h-8 mr-2'/>
@@ -113,28 +124,28 @@ export default function Checkout(){
       </div>
       <h4>¿Desea añadir propina?</h4>  
       <div className='flex justify-between'>
-      <div className="flex bg-white items-center h-full rounded-xl w-full mr-6 p-5 justify-around shadow-xl drop-shadow-md text-gray-800">
+      <div className="flex bg-white items-center h-[77%] rounded-xl w-full mr-6 p-3 justify-around shadow-xl drop-shadow-md text-gray-800">
         <div className='flex flex-col items-center'>
-          <BiHappy className='w-24 h-24 hover:text-gray-600'/>
+          <BiHappy className='w-16 h-16 hover:text-gray-600'/>
           <p>Sin propina</p>
-          <input className='w-8 h-8 accent-green-600'  checked={tip === 0} onClick={()=>handleChange(0)} type="radio" name="tip"></input>
+          <input className='w-6 h-6 accent-green-600'  checked={tip === 0} onClick={()=>handleChange(0)} type="radio" name="tip"></input>
         </div>
         <div>
           <div className='flex flex-col items-center'>
-          <BiHappyBeaming className='w-24 h-24 hover:text-gray-600'/>
+          <BiHappyBeaming className='w-16 h-16 hover:text-gray-600'/>
           <p>5%</p>
-          <input className='w-8 h-8 accent-green-600' onClick={()=>handleChange(0.05)} type="radio" name="tip"></input>
+          <input className='w-6 h-6 accent-green-600' onClick={()=>handleChange(0.05)} type="radio" name="tip"></input>
           </div>
         </div>
         <div>
           <div className='flex flex-col items-center'>
-            <BiHappyHeartEyes className='w-24 h-24 hover:text-gray-600'/>
+            <BiHappyHeartEyes className='w-16 h-16 hover:text-gray-600'/>
             <p>10%</p>
-            <input className='w-8 h-8 accent-green-600' onClick={()=>handleChange(0.1)} type="radio" name="tip"></input>
+            <input className='w-6 h-6 accent-green-600' onClick={()=>handleChange(0.1)} type="radio" name="tip"></input>
           </div>
         </div>
       </div>
-      <div className="flex flex-col bg-white items-center h-[full] w-[70%] rounded-xl p-5 ml-auto text-gray-700  shadow-xl drop-shadow-md">
+      <div className="flex flex-col bg-white items-center h-[95%] w-[70%] rounded-xl p-5 ml-auto text-gray-700  shadow-xl drop-shadow-md">
         <div>
         <div className="flex flex-row rounded-xl justify-between">
           <div>
