c435ca9161786c05f8c5e199ab8b922cb1029947
Nícolas Farfán <nicolas.farfan.ch@usach.cl>
2022-12-05 23:52:00 -0300

    [feat]: Checkout styles

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -7,12 +7,13 @@ import add_quantity from '../utilities/add_quantity';
 import to_clp from '../utilities/to_clp';
 import "../styles/checkout.css"
 import { useNavigate  } from "react-router-dom";
-
+import {BiHappy, BiHappyBeaming, BiHappyHeartEyes} from 'react-icons/bi';
 
 export default function Checkout(){
   const navigate = useNavigate()
   const [cart, setCart] = useState(localStorage.getItem("cart") ? JSON.parse(localStorage.getItem("cart")) : []);
   const [total, setTotal] = useState(0);
+  const [tip, setTip] = useState(0);
 
   useEffect(() => {
     try {
@@ -38,9 +39,11 @@ export default function Checkout(){
   const add_item = (id) => {
     setCart(add_quantity(id))
   }
-
+  const handleChange =  (percentage) => {
+    setTip(percentage);
+  };
   const ProductCart = ({name, quantity, price, img, id}) =>{
-    return  <div className="flex shrink-0 grow-0 w-full h-[100px] my-1 bg-gray-300 rounded-xl items-center justify-between pr-4">
+    return  <div className="flex shrink-0 grow-0 w-full h-[100px] my-3 bg-gray-100 rounded-xl items-center justify-between pr-4 text-gray-800 shadow-xl drop-shadow-md">
     <img className="w-[100px] ml-10 h-[90%]" alt = "pizza" src={img}></img>
     <div className='flex flex-col w-[35%]'>
       <h3 className="text-2xl">{name}</h3>
@@ -57,20 +60,20 @@ export default function Checkout(){
     </div>
   </div>
   }
-  return <div className="flex bg-black h-screen items-center justify-center">
-    <div className="flex flex-col w-[50%] h-[90%] m-3">
-      <div className='flex flex-row bg-gray-300 h-[50%] rounded-xl mb-1.5'>
+  return <div className="flex bg-gray-100 h-screen items-center justify-center">
+    <div className="flex flex-col w-[50%] h-[90%] m-6">
+      <div className='flex flex-row bg-white shadow-xl drop-shadow-md h-[50%] rounded-xl mb-3'>
 
       </div>
-      <div className='flex flex-row bg-gray-300 h-[50%] rounded-xl mt-1.5'>
+      <div className='flex flex-row bg-white shadow-xl drop-shadow-md h-[50%] rounded-xl mt-3'>
 
 </div>
     </div>
-    <div className="flex flex-col w-[70%] h-[90%] ">
-      <div className="flex flex-col flex-nowrap bg-white rounded-xl w-full h-[90%] p-4 overflow-y-auto mb-3">
+    <div className="flex flex-col w-[70%] h-[90%] mr-6 ">
+      <div className="flex flex-col flex-nowrap bg-white rounded-xl w-full h-[90%] p-4 overflow-y-auto mb-6 shadow-xl drop-shadow-md">
         <div className="flex justify-between">
 
-          <div className='flex flex-row text-start items-center mt-[-10px] cursor-pointer text-purple-500' onClick={()=>navigate("/forkpizzas")}>
+          <div className='flex flex-row text-start items-center mt-[-10px] cursor-pointer text-green-500' onClick={()=>navigate("/forkpizzas")}>
             <BsFillArrowLeftCircleFill className='w-8 h-8 mr-2'/>
             <h2 className="underline decoration-wavy">Volver al menú</h2>
           </div>
@@ -83,48 +86,67 @@ export default function Checkout(){
         <ProductCart key = {key} name = {item.name} quantity = {item.quantity} img = {item.img} id = {item.id} price = {item.price}/>): "Cart is empty"}
       </div>
       <div className='flex justify-between'>
-      <div className="flex flex-col bg-[#22C55E] items-end h-full rounded-xl w-full mr-3 p-5">
+      <div className="flex bg-white items-center h-full rounded-xl w-full mr-6 p-5 justify-around shadow-xl drop-shadow-md">
+        <div className='flex flex-col items-center'>
+          <BiHappy className='w-24 h-24'/>
+          <p>Sin propina</p>
+          <input className='w-8 h-8 accent-green-600' onClick={()=>handleChange(0)} type="radio" name="tip"></input>
         </div>
-      <div className="flex flex-col bg-[#e45f13] items-end h-full rounded-xl p-5 ml-auto">
+        <div>
+          <div className='flex flex-col items-center'>
+          <BiHappyBeaming className='w-24 h-24'/>
+          <p>5%</p>
+          <input className='w-8 h-8 accent-green-600' onClick={()=>handleChange(0.05)} type="radio" name="tip"></input>
+          </div>
+        </div>
+        <div>
+          <div className='flex flex-col items-center'>
+            <BiHappyHeartEyes className='w-24 h-24'/>
+            <p>10%</p>
+            <input className='w-8 h-8 accent-green-600' onClick={()=>handleChange(0.1)} type="radio" name="tip"></input>
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-col bg-white items-center h-[full] w-[70%] rounded-xl p-5 ml-auto text-gray-700  shadow-xl drop-shadow-md">
         <div>
         <div className="flex flex-row rounded-xl justify-between">
           <div>
-          <h3 className="text-xl mr-5 text-start">Sub-Total</h3>
+          <h3 className="text-lg mr-5 text-start">Sub-Total: </h3>
           </div>
           <div>
-          <h3 className="text-2xl w-20 mr-10">{to_clp(total)}</h3>  
+          <h3 className="text-lg w-20 mr-10">{to_clp(total)}</h3>  
           </div>
           
         </div>
-        <div className="flex flex-row rounded-xl justify-between">
+        <div className="flex flex-row rounded-xl justify-between my-3">
         <div>
-          <h3 className="text-xl mr-5 text-start">Delivery</h3>
+          <h3 className="text-lg mr-5 text-start">Delivery:</h3>
           </div>
           <div>
-          <h3 className="text-2xl w-20 mr-10">{to_clp(total)}</h3>  
+          <h3 className="text-lg w-20 mr-10">{to_clp(total)}</h3>  
           </div>
         </div>
-        <div className="flex flex-row rounded-xl justify-between">
+        <div className="flex flex-row rounded-xl justify-between mb-2">
         <div>
-          <h3 className="text-xl mr-5 text-start">Propina</h3>
+          <h3 className="text-lg mr-5 text-start">Propina:</h3>
           </div>
           <div>
-          <h3 className="text-2xl w-20 mr-10">{to_clp(total)}</h3>  
+          <h3 className="text-lg w-20 mr-10">{to_clp(total * tip)}</h3>  
           </div>
         </div>
         <div>
-        <hr className='bg-gray-400 h-[2px] mb-3'></hr>
+        <hr className='bg-gray-900 h-[2px] mb-3'></hr>
           </div>
         
-        <div className="flex flex-row rounded-xl justify-between text-white mt-2">
+        <div className="flex flex-row rounded-xl justify-between mt-2">
         <div>
-          <h3 className="text-2xl mr-5 text-start">Total</h3>
+          <h3 className="text-xl mr-5 my-2 text-start">Total  . . . . . . . . . . . . . . .</h3>
           </div>
           <div>
           <h3 className="text-2xl w-20 mr-10">{to_clp(total)}</h3>  
           </div>
         </div>
-        <button type="button" className="font-sans text-xl w-72 mt-2 py-2 bg-black text-white font-medium leading-tight uppercase rounded shadow-md hover:bg-purple-800 hover:shadow-lg active:bg-yellow-700 active:shadow-lg transition duration-150 ease-in-out">
+        <button type="button" className="font-sans text-xl w-full mt-2 py-2 bg-green-500 text-white font-medium leading-tight uppercase rounded shadow-md hover:bg-green-400 hover:shadow-lg active:bg-yellow-700 active:shadow-lg transition duration-150 ease-in-out">
           Pagar
         </button>
         </div>
